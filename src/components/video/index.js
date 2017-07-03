8e9bd9c8b57afba84414c4708cbec4e89a5692bb
meandmycode <me@meandmycode.com>
2017-07-03 15:37:50 +0100

    update video view with component markup

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -9,14 +9,14 @@ import styles from './style.css';
 const getEmbedUri = video => `https://www.youtube.com/embed/${video.id}`;
 
 export default ({ video }) => (
-    <div class={styles.container}>
+    <div class={styles.container} component='video'>
         <div class={styles.details}>
-            <div class={styles.title} href={`/v/${video.id}?rv=`}>{video.snippet.title}</div>
-            <div class={styles.published}>Published on {shortDateFormatter.format(video.snippet.publishedAt)}</div>
-            <RichText class={styles.description} text={video.snippet.description} />
+            <div class={styles.title} part='title'>{video.snippet.title}</div>
+            <div class={styles.published} part='published'>Published on {shortDateFormatter.format(video.snippet.publishedAt)}</div>
+            <RichText class={styles.description} part='description' text={video.snippet.description} />
         </div>
         <div class={styles.media}>
-            <iframe class={styles.video} src={getEmbedUri(video)} />
+            <iframe class={styles.video} part='embed' src={getEmbedUri(video)} />
         </div>
     </div>
 );
