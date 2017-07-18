7e24803bc9e4bad943e59e49a849e2e0ab976b82
Stephen Taylor <me@meandmycode.com>
2017-07-18 14:09:19 +0100

    update build script to skip using rimraf
    we use a pre hook instead to remove the dist

--- scripts/build.js
+++ scripts/build.js
@@ -1,61 +1,52 @@
 /* global process */
-/* eslint-disable no-console, import/no-extraneous-dependencies */
 import { writeFileSync } from 'fs';
-import rimraf from 'rimraf';
+import minimist from 'minimist';
 import webpack from 'webpack';
 
 import configure from '../webpack.config';
+
+const args = minimist(process.argv.slice(2));
+
+const outputPath = args['out-dir'];
 
 const appConfig = {
     youtubeKey: process.env.YOUTUBE_API_KEY,
 };
 
-const config = configure({ production: true, appConfig });
+const config = configure({ production: true, outputPath, appConfig });
 
 const statsOptions = {
     colors: true,
 };
 
-rimraf(config.output.path, err => {
+webpack(config, (err, stats) => {
+
+    const hasErrors = err != null || (stats && stats.hasErrors());
 
     if (err) {
 
         console.error(err.stack || err);
 
+        if (err.details) console.error(err.details);
+
+    }
+
+    if (stats) process.stdout.write(stats.toString(statsOptions));
+
+    if (hasErrors) {
+
+        console.log('\n');
+        console.log('✖️  Web application build failed!');
+        console.log('\n');
+
         return process.exit(1);
 
     }
 
-    webpack(config, (err, stats) => {
+    console.log('\n');
+    console.log('🌟  Web application build successful!');
+    console.log('\n');
 
-        const hasErrors = err != null || (stats && stats.hasErrors());
-
-        if (err) {
-
-            console.error(err.stack || err);
-
-            if (err.details) console.error(err.details);
-
-        }
-
-        if (stats) process.stdout.write(stats.toString(statsOptions));
-
-        if (hasErrors) {
-
-            console.log('\n');
-            console.log('✖️  Web application build failed!');
-            console.log('\n');
-
-            return process.exit(1);
-
-        }
-
-        console.log('\n');
-        console.log('🌟  Web application build successful!');
-        console.log('\n');
-
-        writeFileSync('stats.json', JSON.stringify(stats.toJson()), null, 2);
-
-    });
+    writeFileSync('stats.json', JSON.stringify(stats.toJson()), null, 2);
 
 });
