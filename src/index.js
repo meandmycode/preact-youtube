3a995fb6017d0108d0a404d182237638fe064ea3
Stephen Taylor <me@meandmycode.com>
2017-07-19 22:46:40 +0100

    inline app container into src index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,62 @@ import 'core-js/es6/promise';
 import 'core-js/es6/symbol';
 import 'core-js/es7/symbol';
 import 'regenerator-runtime/runtime';
-import { h, render } from 'preact';
-import App from './containers/app';
+import { h, Component, render } from 'preact';
+import { register } from 'serviceworker-webpack-plugin/lib/runtime';
 
-const serverElement = document.querySelector('div');
+import XhrHttpGetter from './services/xhr-http-getter';
+import { CachingYoutubeService } from './services/youtube';
 
-render(<App />, document.body, serverElement);
+import Router from './components/router';
+import Shell from './components/shell';
+import Playlist from './containers/playlist';
+import Video from './containers/video';
+
+const youtubeService = new CachingYoutubeService(new XhrHttpGetter(), YOUTUBE_API_KEY); // eslint-disable-line no-undef
+const defaultPlaylistId = 'PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ';
+
+class App extends Component {
+
+    progressions = new Map();
+
+    state = {
+        busy: false,
+    }
+
+    handleProgress = e => {
+
+        // devnote: ideally here we would use progress details to show
+        // the actual loaded amount progress, however there are bugs
+        // across various browsers where the loading size cannot be
+        // established due to various reasons (such as content encoding)
+
+        if (e.type === 'loadend') this.progressions.delete(e.target);
+        else this.progressions.set(e.target, e.type);
+
+        const busy = this.progressions.size > 0;
+
+        this.setState({ busy });
+
+    }
+
+    render(_, { busy }) {
+
+        return (
+            <Shell busy={busy}>
+                <Router>
+                    <Playlist path='/p/:playlistId' youtubeService={youtubeService} onProgress={this.handleProgress} />
+                    <Video path='/v/:videoId' youtubeService={youtubeService} onProgress={this.handleProgress} />
+                    <Playlist path='/baby-legs'  youtubeService={youtubeService} onProgress={this.handleProgress} playlistId='PLNu47mcqeyiATtjW5pIRWlpXBu4pUezdP' />
+                    <Playlist default youtubeService={youtubeService} onProgress={this.handleProgress} playlistId={defaultPlaylistId} />
+                </Router>
+            </Shell>
+        );
+    }
+
+}
+
+if ('serviceWorker' in navigator) {
+    register();
+}
+
+render(<App />, document.body, document.querySelector('div'));
